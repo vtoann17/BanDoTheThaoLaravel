c2071523f00fd998e30e84b354d3368cf9267903
nguyenquanghuyhoang <hoangnqhpk04092@gmail.com>
2026-03-25 23:43:33 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
@@ -30,5 +30,6 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
+
 
 ];
